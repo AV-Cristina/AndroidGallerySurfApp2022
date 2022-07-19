2d7b9154e55f7ddcad3710617664fa0cffc051ff
cristina <afanasyeva.kv@open.ru>
2022-07-20 01:59:21 +0300

    Splash screen

---
+++ app/src/main/java/com/cristina/cristinagallery/presentation/SplashFragment.kt
@@ -0,0 +1,32 @@
+package com.cristina.cristinagallery.presentation
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.cristina.cristinagallery.R
+
+/**
+ * Сплеш экран
+ */
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_authorizationFragment)
+        }, 800)
+    }
+}
